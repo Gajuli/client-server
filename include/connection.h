26c9cdd3d759a6280676a45b62e4d44aac3f5b51
Vsevolod Sharov <vsevolod.sharov@gmail.com>
2019-03-03 13:51:19 +0300

    Add connection.h
    We have to store it to control
    connections parameters between
    server and client

---
+++ include/connection.h
@@ -0,0 +1,16 @@
+#ifndef CONNECTION_H_
+#define CONNECTION_H_
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SOCKET_NAME      "/tmp/connection.sock"
+#define JOURNAL          "journal.txt"
+#define KILLER_MESSAGE   "Hande hoch"  
+#define MAX_MESSAGE_SIZE 100
+
+#endif
