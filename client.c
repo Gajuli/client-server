e5aecc887d182fe7b838c48b97b79e776d1dc3b6
Vsevolod Sharov <vsevolod.sharov@gmail.com>
2019-03-03 13:59:45 +0300

    Update client.c

--- client.c
+++ client.c
@@ -1,40 +1,54 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include <sys/un.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "include/connection.h"
 
-int main() 
+int main(int argc, char *argv[]) 
 {
-	int sockfd;
-	int len;
+	int sockfd, result;
 	struct sockaddr_un address;
-	int result;
-	char message[40]={0};
+	char message[MAX_MESSAGE_SIZE];
 	
-
-	/*создается соккет клиента*/
+	if (argc < 2)
+	{
+		perror("Message is required. Usage: ./client \'message\'\n");
+		exit(1);
+	}
+	
+	if (!strncpy(message, argv[1], MAX_MESSAGE_SIZE))
+	{
+		perror("Failed to copy a message");
+		exit(2);
+	}
+	
 	sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-	/**/
+	if (sockfd == -1)
+	{
+		perror("Failed to create a socket");
+		exit(3);
+	}
+	
 	address.sun_family = AF_UNIX;
- 	strcpy(address.sun_path, "server_socket");
-	len = sizeof(address);
-	/*коннектимс*/
-	result = connect(sockfd, (struct sockaddr *)&address, len);
+ 	if(!strcpy(address.sun_path, SOCKET_NAME))
+ 	{
+		perror("Failed to copy a socket's name");
+		exit(4);
+	}
+ 	
+	result = connect(sockfd, (struct sockaddr *)&address,
+				sizeof(address));
  	if (result == -1) 
 	{
-  		perror("servak ne zapush'en");
-  		exit(1);
+  		perror("Connection failed");
+  		exit(5);
  	}
-	sockfd
-	:
-		scanf("%s",message);
- 		write(sockfd, &message, 40);
- 		//read(sockfd, &message, 40);
- 		//printf("char from server = %s\n", message);
- 		close(sockfd);
+ 	
+	result = send(sockfd, message, sizeof(message), 0);
+	if (result == -1)
+	{
+		perror("Failed to send a data");
+		exit(6);
+	}
 	
- 	exit(0);
+	close(sockfd);
+	
+ 	return 0;
 }
 
